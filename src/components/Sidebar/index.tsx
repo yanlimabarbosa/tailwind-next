d8d076a209f42f35c0cddbd0e19972a621185f9c
Yan Lima Barbosa <yanbr763@gmail.com>
2025-01-13 00:52:15 -0300

    feat: add SpaceUsedWidget and remove MainNavigation components

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,19 @@
-import { Search } from "lucide-react"
+import { Cog, LifeBuoy, Search } from "lucide-react"
 import { Logo } from "./Logo"
-import { MainNavigation } from "./MainNavigation"
+import { NavItem } from "./nav-item"
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  Users,
+} from "lucide-react"
+import { UsedSpaceWidget } from "./used-space-widget"
 
 export function Sidebar() {
   return (
-    <aside className="gap-2 space-y-6 border-r border-zinc-200 px-5 py-8 dark:border-zinc-600">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8 dark:border-zinc-600">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-3 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm dark:border-zinc-600">
@@ -16,7 +25,23 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+      </div>
+
+      <UsedSpaceWidget />
     </aside>
   )
 }
