7c9e88bff8bc0212639b482c8a5678112470b87d
Yan Lima Barbosa <yanbr763@gmail.com>
2025-01-13 08:57:48 -0300

    feat: add dark theme suport to UsedSpaceWidget and layout changes

--- src/components/Sidebar/used-space-widget.tsx
+++ src/components/Sidebar/used-space-widget.tsx
@@ -1,11 +1,11 @@
 export function UsedSpaceWidget() {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5 dark:bg-zinc-800">
       <div className="space-y-1">
-        <span className="text-sm/5 font-semibold text-violet-700">
+        <span className="text-sm/5 font-semibold text-violet-700 dark:text-violet-100">
           Used Space
         </span>
-        <p className="text-sm/5 text-violet-500">
+        <p className="text-sm/5 text-violet-500 dark:text-violet-300">
           Your team has used 80% of your available space. Need more?{" "}
         </p>
       </div>
@@ -17,13 +17,13 @@ export function UsedSpaceWidget() {
       <div className="space-x-3">
         <button
           type="button"
-          className="text-sm font-medium text-violet-500 hover:text-violet-700"
+          className="text-sm font-medium text-violet-500 hover:text-violet-700 dark:text-violet-200 hover:dark:text-violet-300"
         >
           Dismiss
         </button>
         <button
           type="button"
-          className="text-sm font-medium text-violet-700 hover:text-violet-900"
+          className="text-sm font-medium text-violet-700 hover:text-violet-900 dark:text-violet-100 hover:dark:text-violet-300"
         >
           Upgrade plan
         </button>
